7641c11841a976bfa6f146e50240e6691fd414d1
NikitaJamadar <nikhitajamadar1998@gmail.com>
2022-03-10 18:01:14 +0530

    Mood analyser program using method

---
+++ Mood_Analyzer/MoodAnalyser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string msg)
+        {
+            if (msg.ToLower().Contains("sad"))
+                return "SAD";
+
+            else
+                return "HAPPY";
+        }
+    }
+}
